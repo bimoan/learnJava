4b7b2d18b8f420641ba4edd7bbf2b98413b52783
Bimo Adi Nurzaman <bimo.development@gmail.com>
2018-04-23 15:19:26 +0700

    Update variabel1.java

--- variabel1.java
+++ variabel1.java
@@ -2,7 +2,7 @@ public class variabel1 {
 	
 	public static void main(String []args) {
 		int angka = 1;
-		String nama ="Andi";
+		String nama ="sule";
 		boolean izin = true;
 		
 		System.out.println("Nama Saya Adalah " + nama);
