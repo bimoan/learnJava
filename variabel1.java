eef70bd2fe94cde754385a56efe7214b2ffe406e
bimoan <bimo.development@gmail.com>
2018-04-23 10:50:50 +0700

    Revisi pertama

--- variabel1.java
+++ variabel1.java
@@ -2,7 +2,7 @@ public class variabel1 {
 	
 	public static void main(String []args) {
 		int angka = 1;
-		String nama ="Bimo";
+		String nama ="Andi";
 		boolean izin = true;
 		
 		System.out.println("Nama Saya Adalah " + nama);
